9b5645bc8b7c484ef56dacbf2ea605188311619b
ttempaa <ttempaa.mt@gmail.com>
2022-10-10 21:08:59 +0300

    Created index file

---
+++ source/index.ts
@@ -0,0 +1,3 @@
+export { getTiles } from '#root/functions/getTiles.js';
+export { getTileInfo } from '#root/functions/getTileInfo.js';
+export { getTileImageUrls } from '#root/functions/getTileImageUrls.js';
